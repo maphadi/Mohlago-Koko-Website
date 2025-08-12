6c9934932dcd3344367329373d58dd350538cd82
Maphadi <26493799@sun.ac.za>
2025-08-13 01:19:34 +0200

    MOre improvements

--- app.js
+++ app.js
@@ -51,38 +51,38 @@ function showSlider(type){
 
 // ------------------------------Counter belt ---------------------------//
 
-const counters = document.querySelectorAll(".counters span");
-const container = document.querySelector(".counters");
+// const counters = document.querySelectorAll(".counters span");
+// const container = document.querySelector(".counters");
 
-let activated = false;
+// let activated = false;
 
-window.addEventListener("scroll", () => {
-    if(
-        pageYOffset > container.offsetTop - container.offsetHeight - 200 && activated == false
-    ){
-        counters.forEach(counter => { 
-            counter.innerText = 0;
-            let count = 0;
-            function updateCount(){
-                const target = parseInt(counter.dataset.count);
-                if(count < target){
-                    count++;
-                    counter.innerText = count;
-                    setTimeout(updateCount, 10);
-                }else{
-                    counter.innerText = target;
-                }
-            }
-            updateCount();
-            activated = true;
-        });
-    } else if(
-        pageYOffset < container.offsetTop - container.offsetHeight - 500 || pageYOffset === 0 && activated === true
-    ) {
-        counters.forEach(counter => {
-            counter.innerText = 0;
-        });
+// window.addEventListener("scroll", () => {
+//     if(
+//         pageYOffset > container.offsetTop - container.offsetHeight - 200 && activated == false
+//     ){
+//         counters.forEach(counter => { 
+//             counter.innerText = 0;
+//             let count = 0;
+//             function updateCount(){
+//                 const target = parseInt(counter.dataset.count);
+//                 if(count < target){
+//                     count++;
+//                     counter.innerText = count;
+//                     setTimeout(updateCount, 10);
+//                 }else{
+//                     counter.innerText = target;
+//                 }
+//             }
+//             updateCount();
+//             activated = true;
+//         });
+//     } else if(
+//         pageYOffset < container.offsetTop - container.offsetHeight - 500 || pageYOffset === 0 && activated === true
+//     ) {
+//         counters.forEach(counter => {
+//             counter.innerText = 0;
+//         });
 
-        activated = false;
-    }
-});
+//         activated = false;
+//     }
+// });
